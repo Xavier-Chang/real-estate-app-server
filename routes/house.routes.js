1a162a0ca857df1f08ca9bf4c8a0ac81cde74975
Lai In Chang <lavender880331@gmail.com>
2023-04-18 12:48:27 +0200

    cloudinary set

--- routes/house.routes.js
+++ routes/house.routes.js
@@ -1,35 +1,65 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const multer = require("multer");
+const cloudinary = require("../cloudinary.config");
 
 const House = require("../models/House.model");
 
-//  POST /api/houses  -  Creates a new house
-router.post("/houses", (req, res, next) => {
-  const {
-    image,
-    price,
-    rooms,
-    size,
-    description,
-    location,
-    constructionYear,
-    hasGarage,
-    madeByMe,
-  } = req.body;
+const upload = multer({ storage: multer.memoryStorage() });
 
-  House.create({ 
-    image,
-    price,
-    rooms,
-    size,
-    description,
-    location,
-    constructionYear,
-    hasGarage,
-    madeByMe })
-    .then((response) => res.json(response))
-    .catch((err) => res.json(err));
+//  POST /api/houses  -  Creates a new house
+router.post("/houses", upload.single("housePicture"), async (req, res, next) => {
+  try {
+    const {
+      price,
+      size,
+      description,
+      constructionYear,
+      hasGarage,
+      madeByMe,
+    } = req.body;
+
+    const rooms = {
+      bedrooms: parseInt(req.body['rooms.bedrooms'], 10),
+      bathrooms: parseInt(req.body['rooms.bathrooms'], 10),
+    };
+
+    const location = {
+      street: req.body['location.street'],
+      city: req.body['location.city'],
+      zip: req.body['location.zip'],
+    };
+
+    // Check if the image file exists in the request
+    if (!req.file) {
+      return res.status(400).json({ message: "Image file is missing" });
+    }
+
+    // Upload the image to Cloudinary
+    const result = await cloudinary.uploader.upload(
+      `data:${req.file.mimetype};base64,${req.file.buffer.toString("base64")}`, // Add the mimetype and prefix
+      { resource_type: "image" }
+    );
+
+    // Create a new house with the uploaded image URL
+    const house = await House.create({
+      image: result.secure_url,
+      price,
+      rooms,
+      size,
+      description,
+      location,
+      constructionYear,
+      hasGarage,
+      madeByMe,
+    });
+
+    res.json(house);
+  } catch (err) {
+    console.error("Error in POST /api/houses:", err);
+    res.status(500).json({ message: "Internal server error", error: err });
+  }
 });
 
 //  GET /api/houses -  Retrieves all of the houses
